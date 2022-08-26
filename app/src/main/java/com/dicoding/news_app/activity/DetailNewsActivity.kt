c63b200f08faf36bca0f01cede146ba8b94f4d43
Erpan Pamuji <lee.pamji@gmail.com>
2022-08-26 21:49:55 +0700

    add detail feature

---
+++ app/src/main/java/com/dicoding/news_app/activity/DetailNewsActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.news_app.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.news_app.R
+
+class DetailNewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_news)
+    }
+}
